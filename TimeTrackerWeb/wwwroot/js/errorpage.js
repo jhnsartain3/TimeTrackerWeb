b98e5e30c1c49bdfd121a4c5c71e7fa48d3894bd
jhnsartain3 <jhnsartain3@gmail.com>
2020-12-13 14:57:16 -0600

    move page javascript to file

---
+++ TimeTrackerWeb/wwwroot/js/errorpage.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("showActualErrorButton").addEventListener("click", ShowEntireError);
+
+function ShowEntireError() {
+    document.getElementById('actualIssueDiv').style.visibility = "visible";
+    document.getElementById('userFriendlyIssueDiv').style.visibility = "hidden";
+    document.getElementById('userFriendlyIssueDiv').style.height = 0;
+}
